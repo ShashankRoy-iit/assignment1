4eec042e0fbafcb37cbdee20e7b15d1596dbe041
ShashankRoy-iit <shashank_24a12res621@iitp.ac.in>
2025-06-13 06:46:53 +0530

    initial commit

---
+++ q2a1.py
@@ -0,0 +1,8 @@
+#Personalized greeting
+
+First_n = input("Enter  your first name: ")
+last_n = input("Enter your last name: ")
+
+full_n = First_n + " " +last_n
+
+print("Hello,", full_n,"! Welcome to Python Program")
